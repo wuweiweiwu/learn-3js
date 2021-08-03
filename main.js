313dcab58b4796490f9b9f75f79a464e5ff0fd49
wei <wei-wei@nashi.io>
2021-08-02 17:51:21 -0700

    SOME MOUSE EVENTS

--- main.js
+++ main.js
@@ -57,7 +57,9 @@ gui.add(material, "wireframe");
 
 const mesh = new THREE.Mesh(geometry, material);
 
-gui.add(mesh.position, "y").min(-3).max(3).step(0.01).name("elevation");
+gui.add(mesh.position, "y").min(-3).max(3).step(0.01).name("x");
+gui.add(mesh.position, "x").min(-3).max(3).step(0.01).name("y");
+gui.add(mesh.position, "z").min(-3).max(3).step(0.01).name("z");
 gui.add(mesh, "visible");
 
 // mesh.position.x = 0.7;
@@ -72,6 +74,20 @@ mesh.rotation.x = Math.PI * 0.25;
 mesh.rotation.y = Math.PI * 0.25;
 
 scene.add(mesh);
+
+// mouse events shenanigans
+const raycaster = new THREE.Raycaster();
+const mouse = new THREE.Vector2();
+
+function onMouseMove(event) {
+  // calculate mouse position in normalized device coordinates
+  // (-1 to +1) for both components
+
+  mouse.x = (event.clientX / window.innerWidth) * 2 - 1;
+  mouse.y = -(event.clientY / window.innerHeight) * 2 + 1;
+}
+
+window.addEventListener("mousemove", onMouseMove, false);
 
 // Sizes
 const sizes = {
@@ -134,6 +150,14 @@ window.addEventListener("dblclick", () => {
   }
 });
 
+// window.addEventListener("mouseover", () => {
+//   material.color.set(0xdeadbeef);
+// });
+
+// window.addEventListener("mouseout", () => {
+//   material.color.set(0xf0f0f0f9);
+// });
+
 /**
  * Animate
  */
@@ -148,6 +172,24 @@ const controls = new OrbitControls(camera, canvas);
 controls.enableDamping = true;
 
 const tick = () => {
+  // update the picking ray with the camera and mouse position
+  raycaster.setFromCamera(mouse, camera);
+
+  // scene.children.forEach((c) => {
+  // console.log("child", c);
+  // c.material.color.set(0xff0000);
+  // });
+
+  material.color.set(0xff0000);
+
+  // calculate objects intersecting the picking ray
+  const intersects = raycaster.intersectObjects(scene.children);
+
+  for (let i = 0; i < intersects.length; i++) {
+    // console.log("intersect", intersects[i]);
+    intersects[i].object.material.color.set(0x00ff00);
+  }
+
   // Update objects
   // mesh.rotation.y += 0.01;
 
